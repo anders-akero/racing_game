da98fa79dfc61d6b5fe90508dc0c2ffb0857b9d9
Anders Åkerö <anders.akero@motionpicturesolutions.com>
2017-08-16 20:29:13 +0100

    Changing Die and Player files to use the class syntax

--- model/Die.js
+++ model/Die.js
@@ -1,18 +1,19 @@
-// Constructor
-function Die(sizes = 6) {
-    // always initialize all instance properties
-    this.sizes = sizes;
+class Die {
+    constructor(sizes = 6) {
+        // always initialize all instance properties
+        this.sizes = sizes;
+    }
+
+    // class methods
+    roll() {
+        this.result = (Math.floor(Math.random() * this.sizes) + 1);
+        return this.result;
+    }
+
+    getResult() {
+        return this.result;
+    }
 }
-
-// class methods
-Die.prototype.roll = function () {
-    this.result = (Math.floor(Math.random() * this.sizes) + 1);
-    return this.result;
-};
-
-Die.prototype.getResult = function() {
-    return this.result;
-};
 
 // export the class
 module.exports = Die;
--- model/Player.js
+++ model/Player.js
@@ -2,57 +2,57 @@ var _ = require("lodash");
 var Die = require('./Die');
 var sprintf = require("sprintf-js").sprintf;
 
-var Player = function (data) {
-    this.data = this.sanitize(data);
-    console.log(sprintf(
-        '%s joins the game',
-        this.data.name
-    ));
-};
-
-Player.prototype.data = {};
-
-Player.prototype.sanitize = function (data) {
-    data = data || {};
-    structure = {name: 'Unknown'};
-    return _.pick(_.defaults(data, structure), _.keys(structure));
-};
-
-Player.prototype.changeName = function (name) {
-    console.log(sprintf(
-        'Player "%s" changed name to "%s"',
-        this.data.name,
-        name
-    ));
-    this.data.name = name;
-    return true;
-};
-
-Player.prototype.get = function (name) {
-    if(typeof this.data[name] === 'undefined') {
-        throw new ReferenceError('Unknown property: ' + name);
+class Player {
+    constructor(data) {
+        this.data = this._sanitize(data);
+        console.log(sprintf(
+            '%s joins the game',
+            this.data.name
+        ));
     }
-    return this.data[name];
-};
 
-Player.prototype.set = function (name, value) {
-    if(typeof this.data[name] === 'undefined') {
-        throw new ReferenceError('Unknown property: ' + name);
+    _sanitize(data) {
+        data = data || {};
+        var structure = {name: 'Unknown'};
+        return _.pick(_.defaults(data, structure), _.keys(structure));
     }
-    this.data[name] = value;
-};
 
-Player.prototype.roll = function () {
-    var die = new Die();
-    die.roll();
+    changeName(name) {
+        console.log(sprintf(
+            'Player "%s" changed name to "%s"',
+            this.data.name,
+            name
+        ));
+        this.data.name = name;
+        return true;
+    }
 
-    console.log(sprintf(
-        '%1$s roll the die. Result is: %2$d',
-        this.get('name'),
-        die.getResult()
-    ));
-    return die.getResult();
-};
+    get(name) {
+        if (typeof this.data[name] === 'undefined') {
+            throw new ReferenceError('Unknown property: ' + name);
+        }
+        return this.data[name];
+    }
+
+    set(name, value) {
+        if (typeof this.data[name] === 'undefined') {
+            throw new ReferenceError('Unknown property: ' + name);
+        }
+        this.data[name] = value;
+    }
+
+    roll() {
+        var die = new Die();
+        die.roll();
+
+        console.log(sprintf(
+            '%1$s roll the die. Result is: %2$d',
+            this.get('name'),
+            die.getResult()
+        ));
+        return die.getResult();
+    }
+}
 
 module.exports = Player;
 
