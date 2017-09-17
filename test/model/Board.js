d4880e46dc4751a765b9a4f5260d235dad9266f4
Anders Åkerö <anders.akero@motionpicturesolutions.com>
2017-09-17 20:39:46 +0100

    Changing a lot of responsibillity of Board to Game

--- test/model/Board.js
+++ test/model/Board.js
@@ -7,83 +7,24 @@ var Player = require('./../../model/Player');
 
 describe('Board', function () {
     describe('#constructor()', function () {
-        it('should give the board a defaultname', function () {
-            var board = new Board();
-            expect(board.name).to.be.a('string').that.equal('My racing track');
+        it('should give you a board of the game', function () {
+            var board = new Board;
+            expect(board.name).to.be.a('string').that.equal('Racing game');
         });
     });
 
-    describe('magic set name', function () {
-        it('should return the name with the first character as uppercase', function () {
-            var board = new Board();
-            board.name = 'new gamename';
-            expect(board.name).to.be.a('string').that.equal('New gamename');
-        });
-    });
-
-    describe('#addPlayer', function () {
-        describe('invalid player', function () {
-            it('should throw an exception', function () {
-                var addingAnInvalidPlayer = function () {
-                    var board = new Board();
-                    board.addPlayer({});
-                };
-                expect(addingAnInvalidPlayer).to.throw(Error, 'player must be of instance Player');
+    describe('name', function () {
+        describe('can be selected', function () {
+            it('should give you the default name of the game', function () {
+                var board = new Board;
+                expect(board.name).to.be.a('string').that.equal('Racing game');
             });
         });
-        describe('single player', function () {
-            it('should add the new player to the game', function () {
-                var board = new Board();
-                var player1 = new Player();
-                board.addPlayer(player1);
-                expect(board.players).to.be.an('array').that.include(player1);
-                expect(board.players.length).to.equal(1);
-            });
-        });
-        describe('multiple player', function () {
-            it('should add the new players to the game', function () {
-                var board = new Board();
-                var player1 = new Player();
-                var player2 = new Player();
-                var player3 = new Player();
-                board.addPlayer(player1);
-                board.addPlayer(player2);
-                board.addPlayer(player3);
-                expect(board.players).to.be.an('array').that.include(player1).and.include(player2).and.include(player3);
-                expect(board.players.length).to.equal(3);
-            });
-        });
-    });
-
-    describe('#start', function () {
-        describe('without any players', function () {
-            it('should throw an exception', function () {
-                var startingWithoutPlayers = function () {
-                    var board = new Board();
-                    board.start();
-                };
-                expect(startingWithoutPlayers).to.throw(Error, 'No players found');
-            });
-        });
-        describe('starting the game twice', function () {
-            it('should throw an exception', function () {
-                var startingWithoutPlayers = function () {
-                    var board = new Board();
-                    board.addPlayer(new Player);
-                    board.start();
-                    board.start();
-                };
-                expect(startingWithoutPlayers).to.throw(Error, 'The game is already running');
-            });
-        });
-        describe('starting the game correctly', function () {
-            it('should not throw an exception', function () {
-                var startingOnceWithOnePlayer = function () {
-                    var board = new Board();
-                    board.addPlayer(new Player);
-                    board.start();
-                };
-                expect(startingOnceWithOnePlayer).not.to.throw();
+        describe('can not be altered', function () {
+            it('should give you the default name of the game', function () {
+                var board = new Board;
+                board.name = 'foo';
+                expect(board.name).to.be.a('string').that.equal('Racing game');
             });
         });
     });
